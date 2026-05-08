c1313645b8cc49cc81c5362fcc45676ff2bd915e
Emosaddd <pl.maksim.krukovich@gmail.com>
2026-05-08 13:46:53 +0200

    feat(network-config): add gravity network configuration

---
+++ apps/web/src/config/templateConfig.ts
@@ -0,0 +1,26 @@
+import type { TemplateConfig } from './constants.extra'
+
+/**
+ * THIS FILE IS AUTO-GENERATED. DO NOT EDIT.
+ * Generated from /networks/gravity/config.json
+ */
+const TEMPLATE_CONFIG = {
+  EIP155: false,
+  SUPPORTED_VERSIONS: ['1.3.0', '1.4.1'],
+  SAFE_UTILS_SUPPORTED: false,
+  EXTRA_FOOTER_LINKS: [
+    { label: 'X', link: 'https://x.com/Galxe' },
+    { label: 'Discord', link: 'https://discord.com/invite/galxe' },
+  ],
+  IS_LICENSED: false,
+  LOGO_DIMENSIONS: {
+    HEADER: {
+      H: '40px',
+    },
+    WELCOME: {
+      W: '120px',
+      H: '64px',
+    },
+  },
+} as TemplateConfig
+export default TEMPLATE_CONFIG
